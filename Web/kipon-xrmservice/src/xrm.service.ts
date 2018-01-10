37bb30af5742fe9a89db4cb7e4637a5c7d991e40
Kjeld Poulsen <kip@boruto.com>
2018-01-10 14:54:22 +0100

    div

--- Web/kipon-xrmservice/src/xrm.service.ts
+++ Web/kipon-xrmservice/src/xrm.service.ts
@@ -61,12 +61,17 @@ export class XrmService {
     }
 
     getCurrenKey(): Observable<XrmEntityKey> {
+        console.log('ver 0.2.8');
         let params = this.getContext().getQueryStringParameters();
         let result = new XrmEntityKey();
         result.id = params["id"];
         result.entityType = params["typename"];
 
         if (result.id != null) {
+            result.id = decodeURIComponent(result.id).replace('{', '').replace('}', '');
+            if (result.entityType === 'undefined' ||result.entityType == null || result.entityType == '') {
+                result.entityType = this.getQueryStringParameters()["typename"];
+            }
             return new Observable<XrmEntityKey>(obs => obs.next(result));
         }
 
@@ -75,6 +80,10 @@ export class XrmService {
             result.entityType = window.parent["Xrm"]["Page"]["data"]["entity"]["getEntityName"]();
 
             if (result.id != null) {
+                result.id = decodeURIComponent(result.id).replace('{', '').replace('}', '');
+                if (result.entityType === 'undefined' || result.entityType == null || result.entityType == '') {
+                    result.entityType = this.getQueryStringParameters()["typename"];
+                }
                 return new Observable<XrmEntityKey>(obs => obs.next(result));
             }
 
@@ -84,9 +93,13 @@ export class XrmService {
                     result.entityType = window.parent["Xrm"]["Page"]["data"]["entity"]["getEntityName"]();
                     if (result.id != null) {
                         clearInterval(intervalThread);
+                        result.id = decodeURIComponent(result.id).replace('{', '').replace('}', '');
+                        if (result.entityType === 'undefined' || result.entityType == null || result.entityType == '') {
+                            result.entityType = this.getQueryStringParameters()["typename"];
+                        }
                         obs.next(result);
                     }
-                }, 500);
+                }, 800);
             });
         } else {
             return new Observable<XrmEntityKey>(obs => obs.next(result));
@@ -112,5 +125,32 @@ export class XrmService {
         let _id = id.replace("{", "").replace("}", "");
 
         return this.http.get<T>(this.getContext().getClientUrl() + this.apiUrl + entityTypes + "(" + _id + ")" + addFields, options);
+    }
+
+    query<T>(entityTypes: string, fields: string, filter: string): Observable<T[]>;
+    query<T>(entityTypes: string, fields: string, filter: string, orderBy: string): Observable<T[]>;
+    query<T>(entityTypes: string, fields: string, filter: string, orderBy: string, top: number): Observable<T[]>;
+    query<T>(entityTypes: string, fields: string, filter: string, orderBy: string = null, top: number = 250, page: number = 1): Observable<T[]> {
+        let headers = new HttpHeaders({ 'Accept': 'application/json' });
+        headers.append("OData-MaxVersion", "4.0");
+        headers.append("OData-Version", "4.0");
+        headers.append("Content-Type", "application/json; charset=utf-8");
+        headers.append("Prefer", "odata.include-annotations=*");
+
+        let url = this.getContext().getClientUrl() + this.apiUrl + entityTypes;
+
+
+        return null;
+    }
+
+    private getQueryStringParameters(): any {
+        let search = window.location.search;
+        let hashes = search.slice(search.indexOf('?') + 1).split('&');
+        let params = {};
+        hashes.map(hash => {
+            let [key, val] = hash.split('=')
+            params[key] = decodeURIComponent(val)
+        })
+        return params;
     }
 }
